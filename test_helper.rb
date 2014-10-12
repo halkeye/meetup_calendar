d752a7bfb57192f43cdcfb86dd453dacffe54234
Gavin Mogan <gavin@kodekoan.com>
2014-10-11 21:01:43 -0700

    add simplecov-rcov

--- test_helper.rb
+++ test_helper.rb
@@ -1,6 +1,8 @@
 # test_helper.rb
 if ENV['GENERATE_REPORTS'] == 'true'
   require 'simplecov'
+  require 'simplecov-rcov'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
   SimpleCov.start do
     root 'test'
   end
